1de2bcd6a53532af25010e6298b83c96bb98dccf
clembias <clement47bia@hotmail.fr>
2024-11-27 09:02:58 +0100

    Working website

---
+++ javascript-algorithms-data-structures/project1-palindrome-checker/script.js
@@ -0,0 +1,30 @@
+const textInput = document.getElementById("text-input");
+const button = document.getElementById("check-btn");
+
+button.addEventListener("click", function() {
+  
+  let userInput = textInput.value; 
+  if (userInput === "") {
+    alert("Please input a value");
+  } else {
+    
+    let charactersArray = userInput.match(/[A-Za-z0-9]/g) || []; 
+    let charactersArrayReversed = charactersArray.slice().reverse(); 
+
+    
+    let charactersString = charactersArray.join(''); 
+    let charactersStringReversed = charactersArrayReversed.join(''); 
+
+    console.log(charactersString);
+    console.log(charactersStringReversed);
+
+    
+    if (charactersString.toLowerCase() === charactersStringReversed.toLowerCase()) {
+      console.log(userInput + " is a palindrome");
+      document.getElementById("result").innerHTML = `${userInput} is a palindrome`; 
+    } else {
+      console.log(userInput + " is not a palindrome");
+      document.getElementById("result").innerHTML = `${userInput} is not a palindrome`  
+    }
+  }
+});
